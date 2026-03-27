9fb7f5e96a25b56600f08cc3a186b11a35809e09
Kathirvelan K <kathirvelan.k@gmail.com>
2026-03-27 13:57:49 +0530

    gitignore check

--- try.py
+++ try.py
@@ -1,16 +1,6 @@
-class Dog:
-    def sound(self):
-        return "Woof, woof!"
+from flask import Flask
+app = Flask(__name__)
 
-class Cat:
-    def sound(self):
-        return "Meow, meow!"
-
-def make_animal_sound(animal):
-    print(animal.sound())
-
-dog = Dog()
-cat = Cat()
-
-for obj in [dog,cat]:
-    obj.sound()
+@app.route('/')
+def name():
+    return 'HI...THIS IS MY FIRST FLASK APP'
